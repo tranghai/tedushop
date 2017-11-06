3cf5ac829907e062b15c3486c4d3b08c5be001b7
louisluong <louisluong1910@gmail.com>
2017-11-06 17:59:31 +0700

    commit migration and repository

---
+++ TeduShop.Data/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IOrderRepository  : IRepository<Order>
+    {
+    }
+
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ TeduShop.Data/Repositories/PostRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+    }
+
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
+    {
+        public PostRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
