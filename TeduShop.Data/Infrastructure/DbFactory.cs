495cc2d7be336ef40ab613822c5b3ae65b32f234
louisluong <louisluong1910@gmail.com>
2017-11-06 17:27:12 +0700

    add Entity Framerwork Code first for model and data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        TeduShopDbContext dbContext;
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ TeduShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private TeduShopDbContext dbContext;
+        private readonly IDbFactory dbFactory;
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+        public TeduShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = new TeduShopDbContext()); }
+        }
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
